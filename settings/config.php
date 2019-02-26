7490f150345b465f27eb07bf3e16a376236af3f2
Mathias Lantean <mathias.lantean@innuy.com>
2019-02-26 15:24:58 -0300

    Fix MySql server IP

--- settings/config.php
+++ settings/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define("engine","mysql");
-define("server","192.168.56.101");
+define("server","127.0.0.1");
 define("database","club");
 define("dbUser","root");
 define("dbPassword","root");
