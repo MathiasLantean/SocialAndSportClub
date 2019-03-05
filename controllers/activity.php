022b3e5d5b4ccf9198d33e08a877a7d2bcc18f3e
Mathias Lantean <mathias.lantean@innuy.com>
2019-03-05 20:48:23 -0300

    Add API to leave activity

--- controllers/activity.php
+++ controllers/activity.php
@@ -30,4 +30,22 @@ function getAvailableActivities($user_id){
     return null;
 }
 
+function leaveActivity($user_id, $activity_id){
+    $sql = "DELETE FROM enroll WHERE user = :user AND activity = :activity";
+    $params = array();
+    $params[0] = array("user", $user_id, "int");
+    $params[1] = array("activity", $activity_id, "int");
+    $conn = getConnection();
+    
+    error_log(print_r( $sql, TRUE));
+    error_log(print_r( $params[0], TRUE));
+    error_log(print_r( $params[1], TRUE));
+    if ($conn){
+        if($conn->consulta($sql, $params)){
+            return True;
+        }
+    }
+    return False;
+}
+
 ?>
